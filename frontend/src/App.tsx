2e730f2d880fdeaa8f96af268591bdb0c86525f4
jon-chow <jon.chowyyc@gmail.com>
2023-03-17 15:47:37 -0700

    added temp ui

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,25 @@
+import { GiFarmTractor, GiFarmer, GiWheat } from 'react-icons/gi';
+
 function App() {
   return (
     <div className="App">
+      <nav className="NavBar">
+        <GiFarmTractor />
+        <h1>The Bestest Farm Management System&#8482;</h1>
+      </nav>
 
+      <main>
+        {/* CONTROL PANEL */}
+        <div className="ControlPanel">
+          <h2><GiFarmer /> Control Panel</h2>
+        </div>
+
+        {/* DISPLAY PANEL */}
+        <div className="DisplayPanel">
+          <h2><GiWheat /> Display Panel</h2>
+
+        </div>
+      </main>
     </div>
   );
 }
