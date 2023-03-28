581dae7adc083487d1c449d9f4b22db54ddd73c2
AllenZoo <allenzhu2000@hotmail.com>
2023-03-28 15:17:02 -0700

    Update FarmingManagementSystemApplication.java
    yeeeeeet

--- backend/src/main/java/com/main/application/FarmingManagementSystemApplication.java
+++ backend/src/main/java/com/main/application/FarmingManagementSystemApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class FarmingManagementSystemApplication {
 
-    // testing commit user again
+    // yeeeeeeet
     public static void main(String[] args) {
         SpringApplication.run(FarmingManagementSystemApplication.class, args);
     }
