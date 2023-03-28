5b38485bf382ceaf9b57403f275088bd06578afe
jon-chow <jon.chowyyc@gmail.com>
2023-03-27 21:52:30 -0700

    added comment for Allen :)

--- backend/src/main/java/com/example/demo/HelloWorldController.java
+++ backend/src/main/java/com/example/demo/HelloWorldController.java
@@ -7,12 +7,18 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HelloWorldController {
 
+    /**
+     * Hello :)
+     */
     @CrossOrigin(origins = "http://localhost:8080")
     @RequestMapping("/")
     public String helloWorld() {
         return "Hello World from Spring Boot";
     }
 
+    /**
+     * Goodbye :)
+     */
     @CrossOrigin(origins = "http://localhost:8080")
     @RequestMapping("/goodbye")
     public String goodbye() {
