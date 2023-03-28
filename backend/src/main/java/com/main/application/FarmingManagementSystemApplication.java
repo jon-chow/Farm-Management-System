04e2921ecc25a8a9be9a22e0830c873cc0f8ce73
allnzhu <allnzhu@student.ubc.ca>
2023-03-28 15:11:13 -0700

    testing to see if git user changed.

--- backend/src/main/java/com/main/application/FarmingManagementSystemApplication.java
+++ backend/src/main/java/com/main/application/FarmingManagementSystemApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class FarmingManagementSystemApplication {
 
+    // testing commit user
     public static void main(String[] args) {
         SpringApplication.run(FarmingManagementSystemApplication.class, args);
     }
