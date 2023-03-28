cd27844b2ac0a2527f0c20af2d7b5566ce7a72dc
AllenZoo <allenzhu2000@hotmail.com>
2023-03-28 15:13:51 -0700

    testing to see if git user changed. no.2

--- backend/src/main/java/com/main/application/FarmingManagementSystemApplication.java
+++ backend/src/main/java/com/main/application/FarmingManagementSystemApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class FarmingManagementSystemApplication {
 
-    // testing commit user
+    // testing commit user again
     public static void main(String[] args) {
         SpringApplication.run(FarmingManagementSystemApplication.class, args);
     }
