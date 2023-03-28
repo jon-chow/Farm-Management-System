137df5b811f59037ee2e78aa4fd2747a90fb9383
AllenZoo <allenzhu2000@hotmail.com>
2023-03-27 18:56:46 -0700

    fixed small bug. Working REST calls.

--- backendTestDemo/src/main/java/com/example/demo/HelloWorldController.java
+++ backendTestDemo/src/main/java/com/example/demo/HelloWorldController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HelloWorldController {
 
-    @RequestMapping
+    @RequestMapping("/")
     public String helloWorld() {
         return "Hello World from Spring Boot";
     }
