e7c785f6fa484dd9b4cd13ccff8168b67fef2757
jon-chow <jon.chowyyc@gmail.com>
2023-03-28 02:02:48 -0700

    moved farm page, added main page sections

--- frontend/src/pages/MainPage.tsx
+++ frontend/src/pages/MainPage.tsx
@@ -1,4 +1,5 @@
 import styled from 'styled-components';
+import { GiBarn, GiFarmer, GiPerson, GiBroccoli } from 'react-icons/gi';
 
 import NavBar from '@components/NavBar';
 
@@ -11,27 +12,54 @@ const StyledMainPage = styled.div`
   text-align: center;
 
   main {
-    display: flex;
-    flex-direction: row;
+    display: grid;
+    grid-template-columns: repeat(auto-fit, minmax(40vw, 1fr));
     gap: 0.5rem;
-    min-height: 50vh;
-    margin: 1rem 2rem;
+    height: 100%;
+    margin: 0 4rem;
+    padding: 2rem;
+  }
+`;
+
+const StyledSection = styled.section`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 1rem;
+  padding: 2rem;
+  min-height: 20vh;
+  border: 1px solid #fff;
+  border-radius: 1rem;
+  background: rgba(0, 0, 0, 0.1);
+  transition: 0.2s ease;
+
+  h1 {
+    margin: 0;
+    font-size: 2rem;
   }
 
-  .ControlPanel {
-    display: flex;
-    justify-content: center;
-    width: 50%;
-    border-radius: 5px;
-    background-color: rgba(255, 255, 255, 0.25);
+  &:hover {
+    background: rgba(0, 0, 0, 0.2);
+    transition: 0.2s ease;
   }
+`;
 
-  .DisplayPanel {
-    display: flex;
-    justify-content: center;
-    width: 100%;
-    border-radius: 5px;
-    background-color: rgba(255, 255, 255, 0.25);
+const StyledButton = styled.button`
+  color: #fff;
+  font-size: 1.5rem;
+  text-align: center;
+
+  padding: 0.5rem 1rem;
+  min-width: 20rem;
+  border: 2px solid #fff;
+  border-radius: 0.5rem;
+  background: transparent;
+  cursor: pointer;
+  transition: 0.2s ease;
+
+  &:hover {
+    background: rgba(0, 0, 0, 0.1);
+    transition: 0.2s ease;
   }
 `;
 
@@ -40,21 +68,88 @@ const StyledMainPage = styled.div`
 /*                                  COMPONENT                                 */
 /* -------------------------------------------------------------------------- */
 const MainPage = () => {
+
+  const handleTendFields = () => {
+    console.log('Tend Fields');
+  };
+
+  const handleNurtureAnimals = () => {
+    console.log('Nurture Animals');
+  };
+
+  const handleSellProducts = () => {
+    console.log('Sell Products');
+  };
+
+  const handleManageFacilities = () => {
+    console.log('Manage Facilities');
+  };
+
+
   return (
     <StyledMainPage>
       <NavBar />
 
       <main>
-        {/* CONTROL PANEL */}
-        <div className="ControlPanel">
-          <h2>Control Panel</h2>
-        </div>
+        <StyledSection>
+          <h1><GiFarmer size="2rem" /> Farmer Actions</h1>
 
-        {/* DISPLAY PANEL */}
-        <div className="DisplayPanel">
-          <h2>Display Panel</h2>
+          <StyledButton
+            onClick={handleTendFields}
+          >
+            Tend Fields
+          </StyledButton>
 
-        </div>
+          <StyledButton
+            onClick={handleNurtureAnimals}
+          >
+            Nurture Animals
+          </StyledButton>
+
+          <StyledButton
+            onClick={handleSellProducts}
+          >
+            Sell Products
+          </StyledButton>
+
+          <StyledButton
+            onClick={handleManageFacilities}
+          >
+            Manage Facilities
+          </StyledButton>
+        </StyledSection>
+
+        <StyledSection>
+          <h1><GiPerson /> User Info</h1>
+
+          <StyledButton>
+            View Profile
+          </StyledButton>
+
+          <StyledButton>
+            View Inventory
+          </StyledButton>
+        </StyledSection>
+        
+        <StyledSection>
+          <h1><GiBroccoli /> Company Fun Facts</h1>
+
+          <StyledButton>
+            View Company Info
+          </StyledButton>
+        </StyledSection>
+        
+        <StyledSection>
+          <h1><GiBarn /> Housing</h1>
+
+          <StyledButton>
+            View Housing
+          </StyledButton>
+
+          <StyledButton>
+            Manage Housing
+          </StyledButton>
+        </StyledSection>
       </main>
     </StyledMainPage>
   );
