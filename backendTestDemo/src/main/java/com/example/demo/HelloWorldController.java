775d4a57d9a890168e2d9097f366bfb928ff478d
AllenZoo <allenzhu2000@hotmail.com>
2023-03-27 21:08:00 -0700

    hoping to have fixed cors cross-origin bugs.

--- backendTestDemo/src/main/java/com/example/demo/HelloWorldController.java
+++ backendTestDemo/src/main/java/com/example/demo/HelloWorldController.java
@@ -1,16 +1,19 @@
 package com.example.demo;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloWorldController {
 
+    @CrossOrigin(origins = "http://localhost:8080")
     @RequestMapping("/")
     public String helloWorld() {
         return "Hello World from Spring Boot";
     }
 
+    @CrossOrigin(origins = "http://localhost:8080")
     @RequestMapping("/goodbye")
     public String goodbye() {
         return "Goodbye from Spring Boot";
