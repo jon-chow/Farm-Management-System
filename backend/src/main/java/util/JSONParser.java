e5626eba22a4d5612137fec6556f12af6df8cf1e
AllenZoo <allenzhu2000@hotmail.com>
2023-03-30 14:54:23 -0700

    fixed date parsing

--- backend/src/main/java/util/JSONParser.java
+++ backend/src/main/java/util/JSONParser.java
@@ -6,12 +6,7 @@ public class JSONParser {
 
 
     public Date parseDate(String json) {
-        // JSON is of format: 'yyyy-mm-dd'
-        String year = json.substring( 0, 4);
-        String month = json.substring(5, 7);
-        String day = json.substring(8, 10);
-
-        Date date = new Date(Integer.parseInt(year), Integer.parseInt(month), Integer.parseInt(day));
+        Date date = Date.valueOf(json);
         return date;
     }
 }
