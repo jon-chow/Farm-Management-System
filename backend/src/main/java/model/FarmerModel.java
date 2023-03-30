902a6396a1852ce613339149cb7b941c52714f6c
AllenZoo <allenzhu2000@hotmail.com>
2023-03-29 19:08:47 -0700

    added farmer model.

---
+++ backend/src/main/java/model/FarmerModel.java
@@ -0,0 +1,31 @@
+package model;
+
+public class FarmerModel {
+    private final int farmerID;
+    private final String fullName;
+    private final int yearsOfEmployment;
+    private final int salary;
+
+    public FarmerModel(int farmerID, String fullName, int yearsOfEmployment, int salary) {
+        this.farmerID = farmerID;
+        this.fullName = fullName;
+        this.yearsOfEmployment = yearsOfEmployment;
+        this.salary = salary;
+    }
+
+    public int getFarmerID() {
+        return farmerID;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public int getYearsOfEmployment() {
+        return yearsOfEmployment;
+    }
+
+    public int getSalary() {
+        return salary;
+    }
+}
