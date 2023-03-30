2ed1fe17342a0c0db737154f4b37e6538482a0fa
AllenZoo <allenzhu2000@hotmail.com>
2023-03-29 18:25:57 -0700

    added api calls for login and logout. also refactored some files.

--- backend/src/main/java/com/server/FarmingManagementSystemApplication.java
+++ backend/src/main/java/com/server/FarmingManagementSystemApplication.java
@@ -1,4 +1,4 @@
-package com.main.application;
+package com.server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
---
+++ backend/src/main/java/com/server/controller/MainController.java
@@ -0,0 +1,66 @@
+package com.server.controller;
+
+import ch.qos.logback.core.model.Model;
+import controller.FarmingSystem;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.web.bind.annotation.*;
+
+import java.io.IOException;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+import static org.springframework.web.bind.annotation.RequestMethod.GET;
+import static org.springframework.web.bind.annotation.RequestMethod.POST;
+
+@RestController
+public class MainController {
+
+    private FarmingSystem system;
+
+    public MainController() {
+        system = new FarmingSystem();
+    }
+
+    /**
+     * Hello :)
+     */
+    @CrossOrigin(origins = "http://localhost:8080")
+    @RequestMapping("/")
+    public String helloWorld() {
+        return "Hello World from Spring Boot";
+    }
+
+    /**
+     * Goodbye :)
+     */
+    @CrossOrigin(origins = "http://localhost:8080")
+    @RequestMapping("/goodbye")
+    public String goodbye() {
+        return "Goodbye from Spring Boot";
+    }
+
+
+    @CrossOrigin(origins = "http://localhost:8080")
+    @RequestMapping(value = "/login", method = POST)
+    public boolean login(@RequestBody Map<String, Object> map) {
+        Object username = map.get("username");
+        Object password = map.get("password");
+        System.out.println("Username: " + username + " password: " + password);
+        return system.login(username.toString(), password.toString());
+    }
+
+    @CrossOrigin(origins = "http://localhost:8080")
+    @RequestMapping(value = "/logout", method = GET)
+    public boolean logout() {
+        return system.logout();
+    }
+
+
+    // For Ref!
+    //    @CrossOrigin(origins = "http://localhost:8080")
+    //    @RequestMapping(value = "/login", method = POST)
+    //    public boolean login(HttpServletRequest req, HttpServletResponse res, Model model) throws IOException {
+    //
+    //    }
+}
